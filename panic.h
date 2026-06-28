9c7cc3021fbb94b6abfa443cfc482615317b7899
tangjiaxin188 <kanshenmekanzhaosi@163.com>
2026-06-28 22:41:44 +0800

    内核恐慌

---
+++ panic.h
@@ -0,0 +1,8 @@
+#ifndef PANIC_H
+#define PANIC_H
+#define PANIC(fmt, ...)                                                        \
+    do {                                                                       \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {}                                                           \
+    } while (0)
+#endif
