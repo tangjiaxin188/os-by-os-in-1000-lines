5d8371770df6f0c84b91fc8a8ea1d5d1f65b109a
tangjiaxin188 <kanshenmekanzhaosi@163.com>
2026-06-28 22:42:34 +0800

    用户态库

---
+++ user.h
@@ -0,0 +1,18 @@
+#ifndef USER_H
+#define USER_H
+
+__attribute__((noreturn))
+void exit(void);
+
+__attribute__((section(".text.start")))
+__attribute__((naked))
+void start(void);
+
+void putchar(char ch);
+
+int getchar(void);
+int syscall(int sysno, int arg0, int arg1, int arg2);
+
+#include "common.h"
+
+#endif
